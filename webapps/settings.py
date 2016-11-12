82e8b0a223e42562e0d6e1e19b098c9af01e1370
evelynhuangyt <evelynhuangyt@163.com>
2016-11-11 22:01:38 -0500

    host

--- webapps/settings.py
+++ webapps/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'wr^299+27$&%y28xjvpluz_cobf@h00$)_ek&mus6#gre-+zpn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['0.0.0.0','yh1-webapp.herokuapp.com']
 
 #Login Settings
 LOGIN_URL = '/'
